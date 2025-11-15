c168f63dfb9031be60b560b998804e41c5528584
Turbo Coder <turbocoder13@gmail.com>
2025-11-15 11:15:27 +0100

    feat: implement Jekyll gem wrapper for bulma-turbo-themes (#80)

---
+++ lib/bulma-turbo-themes.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "bulma-turbo-themes/version"
+
+# Jekyll theme for Bulma Turbo Themes
+# Provides accessible theme packs and theme selector for Bulma CSS framework
+module BulmaTurboThemes
+end
+
